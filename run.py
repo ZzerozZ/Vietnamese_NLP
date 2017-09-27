181e747d534aef95ad22170097e90dd06390b53e
Trọng Nghĩa (Bill) <nghiaduong.170697@outlook.com.vn>
2017-09-27 19:27:02 +0700

    Update run.py

--- run.py
+++ run.py
@@ -1,17 +1,14 @@
 # -*- coding: utf-8 -*-
 
-import underthesea
 
-
-from Split_world import is_exist
+from Split_world import is_exist, split_word
 
 print 'KIỂM LỖI CHÍNH TẢ\n'
 
-sentence = u'một nhóm xe ôm truyền thống đánh hội đồng một người chạy GrabBike'
+sentence = u'học sinh học môn giải tích tại lớp học'
 print u'Chuỗi: ', sentence, u'\nKết quả:\n'
-_list = underthesea.word_sent(sentence)
+_list = split_word(sentence)
 
 for word in _list:
     print word.lower(), is_exist(word.lower().encode('utf-8'))
-
 
