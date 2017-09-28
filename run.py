92451e8f6c8262d82992b25043344459befaa2b4
Trọng Nghĩa (Bill) <nghiaduong.170697@outlook.com.vn>
2017-09-28 20:29:40 +0700

    Update run.py

--- run.py
+++ run.py
@@ -1,14 +1,16 @@
 # -*- coding: utf-8 -*-
 
-
-from Split_world import is_exist, split_word
+import time
+from Split_world import split_word
 
 print 'KIỂM LỖI CHÍNH TẢ\n'
-
 sentence = u'học sinh học môn giải tích tại lớp học'
 print u'Chuỗi: ', sentence, u'\nKết quả:\n'
+
+start = time.time()
+
 _list = split_word(sentence)
-
 for word in _list:
-    print word.lower(), is_exist(word.lower().encode('utf-8'))
+    print word.word.lower(), word.correct
 
+print 'Total time: ', time.time() - start
