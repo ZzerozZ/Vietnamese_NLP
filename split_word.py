c35549e3a559b3b93bf4b140d86700f7754b5670
Trọng Nghĩa (Bill) <nghiaduong.170697@outlook.com.vn>
2017-09-28 20:30:07 +0700

    Optimize code

--- split_word.py
+++ split_word.py
@@ -1,41 +1,41 @@
 # -*- coding: utf-8 -*-
 
 
-def get_word(dec):
-    r_value = dec.split('\t')
-    return r_value[0].strip()
+from Word import Word
+from dic_hashing import hash_word, get_hashtable
 
 
-def get_dictionary():
-
-    input_file = open("VDic_uni.txt", "r")
-    dic = []
-    for line in input_file:
-        dic.append(get_word(line))
-    return dic
-
-
-def is_exist(word):
-    dic = get_dictionary()
-    try:
-        dic.index(word)
-        return u'\u2713'
-    except ValueError:
-        return u'\u274C'
+def is_exist(word, hash_table):
+    word = word.strip().lower().decode('utf-8')
+    k = 0
+    pos = hash_word(word)
+    while hash_table[(pos + k * k) % 50000].encode('utf-8') != word.lower().encode('utf-8'):
+        if hash_table[(pos + k * k) % 50000] == ''.encode('utf-8'):
+            return u'\u274C'
+        k += 3
+    return u'\u2713'
 
 
 def split_word(sentence):
     words = sentence.split(' ')
     word_array = []
+    hash_table = get_hashtable()
+
     i = 0
-    while i < len(words):
-        if i + 2 < len(words) and is_exist((words[i] + ' ' + words[i + 1] + ' ' + words[i + 2]).encode('utf-8')) == u'\u2713':
-            word_array.append(words[i] + ' ' + words[i + 1] + ' ' + words[i + 2])
+    len_ = len(words)
+    while i < len_:
+        if i + 2 < len_ and is_exist((words[i] + ' ' + words[i + 1] + ' ' + words[i + 2]).encode('utf-8'), hash_table)\
+                == u'\u2713':
+            word_array.append(Word(words[i] + ' ' + words[i + 1] + ' ' + words[i + 2], u'\u2713'))
             i += 2
-        elif i + 1 < len(words) and is_exist((words[i] + ' ' + words[i + 1]).encode('utf-8')) == u'\u2713':
-            word_array.append(words[i] + ' ' + words[i + 1])
+        elif i + 1 < len_ and is_exist((words[i] + ' ' + words[i + 1]).encode('utf-8'), hash_table) == u'\u2713':
+            word_array.append(Word(words[i] + ' ' + words[i + 1], u'\u2713'))
             i += 1
         else:
-            word_array.append(words[i])
+            if is_exist(words[i].encode('utf-8'), hash_table):
+                word_array.append(Word(words[i], u'\u2713'))
+            else:
+                word_array.append(Word(words[i], u'\u274C'))
         i += 1
+
     return word_array
