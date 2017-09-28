8e4a6b3efab9d5c7da692bf333d0f7f2e93a587b
Trọng Nghĩa (Bill) <nghiaduong.170697@outlook.com.vn>
2017-09-28 21:55:47 +0700

    Add special string format chẹcker

--- spell_checker/Word.py
+++ spell_checker/Word.py
@@ -1,4 +1,38 @@
+from dic_hashing import to_int
+
+
 class Word:
     def __init__(self, _word, _correct):
         self.word = _word
         self.correct = _correct
+
+
+def special_type(word):
+    if is_mail_address(word) == 1:
+        word = '?isName?'
+    if is_marker(word) == 1:
+        word = '?isDigit?'
+    return word
+
+
+def is_mail_address(word):
+    try:
+        is_email = word.index('@')
+        for i in range(0, is_email + 1):
+            if word[i] != '.' and to_int(word) == 0:
+                return 0
+
+        for i in range(is_email + 2, len(word)):
+            if word[i] != '.' and to_int(word) == 0:
+                return 0
+
+        return 1
+    except ValueError:
+        return 0
+
+
+def is_marker(word):
+    if word == "'" or word == '"' or word == '.' or word == ',' or word == '...' or word == '?' or word == '!'\
+            or word == ':' or word == ';' or word == '-':
+        return 1
+    return 0
